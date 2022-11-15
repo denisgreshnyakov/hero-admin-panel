27f3b90d64fa5ad29ac129062d622d1c91822bd6
Denis <denisgreshnaykov@gmail.com>
2022-11-15 22:00:06 +0500

    first commit

---
+++ src/components/heroesList/HeroesList.js
@@ -0,0 +1,57 @@
+import { useHttp } from "../../hooks/http.hook";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
+
+import {
+  heroesFetching,
+  heroesFetched,
+  heroesFetchingError,
+} from "../../actions";
+import HeroesListItem from "../heroesListItem/HeroesListItem";
+import Spinner from "../spinner/Spinner";
+
+// Задача для этого компонента:
+// При клике на "крестик" идет удаление персонажа из общего состояния
+// Усложненная задача:
+// Удаление идет и с json файла при помощи метода DELETE
+
+const HeroesList = () => {
+  const { heroes, heroesLoadingStatus } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const { request } = useHttp();
+
+  useEffect(() => {
+    dispatch(heroesFetching());
+    request("http://localhost:3001/heroes")
+      .then((data) => dispatch(heroesFetched(data)))
+      .catch(() => dispatch(heroesFetchingError()));
+
+    // eslint-disable-next-line
+  }, []);
+
+  if (heroesLoadingStatus === "loading") {
+    return <Spinner />;
+  } else if (heroesLoadingStatus === "error") {
+    return <h5 className="text-center mt-5">Ошибка загрузки</h5>;
+  }
+
+  const renderHeroesList = (arr) => {
+    if (arr.length === 0) {
+      return <h5 className="text-center mt-5">Героев пока нет</h5>;
+    }
+
+    console.log(arr[0].id);
+
+    return arr.map(({ id, ...props }) => {
+      //id = uuidv4();
+
+      return <HeroesListItem key={id} {...props} />;
+    });
+  };
+
+  const elements = renderHeroesList(heroes);
+  return <ul>{elements}</ul>;
+};
+
+export default HeroesList;
